df68e4db01714f91e5b287446c81b6c3e1eb1141
jasonntuuwa <jasonntuuwa@gmail.com>
2026-07-16 14:35:23 -0500

    Add event listeners for adding and deleting chapters

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,25 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
-const li = document.createElement('li')
-const deleteButton = document.createElement('button');
-li.textContent = input.value; 
-deleteButton.textContent = '❌'
-li.append(deleteButton);
-list.append(li);
+
+button.addEventListener('click', function(){
+    if (input.value === '') {
+        input.focus();
+        return;
+    }
+
+
+    const li = document.createElement('li')
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌'
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+        li.remove();
+    });
+
+    input.value = '';
+    input.focus();
+});
